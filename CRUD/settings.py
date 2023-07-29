408e60dcacb326c10d8f66d08342ef17d8ed4f05
Mohanraj <mohanmohanraj136@gmail.com>
2023-07-30 04:26:07 +0530

    file modified static

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 MEIDA_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     BASE_DIR/'static/'
