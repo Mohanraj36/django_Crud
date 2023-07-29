31d5bd2eb73b0e02a1f01b9cd1a3f042aaf3b009
Mohanraj <mohanmohanraj136@gmail.com>
2023-07-30 05:03:19 +0530

    j

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -126,11 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
-MEDIA_URL = '/media/' # Public URL at the browser
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
