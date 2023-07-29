a2f34eff1e686ed50e9681c2c92a51e07764a761
Mohanraj <mohanmohanraj136@gmail.com>
2023-07-30 05:08:13 +0530

    gfhg

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
+MEDIA_URL = '/media/' # Public URL at the browser
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
